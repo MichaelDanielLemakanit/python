0bd62374f05f475745116d7da3277a6ea42ecaaf
Daniel michael <michaellemakanit@gmail.com>
2026-04-24 14:13:24 +0300

    second commit

--- task3.py
+++ task3.py
@@ -35,7 +35,6 @@ else:
     print("Other email provider")
 
 
-
 # 3.
 # Write a program that checks password strength:
 # "Weak" if length < 6
@@ -43,6 +42,19 @@ else:
 # "Strong" if length > 10 and contains both digits and uppercase letters
 
 
+password = input("Enter your password: ")
+
+if len(password) < 6:
+    print("Weak")
+
+elif 6 <= len(password) <= 10 and any(char.isdigit() for char in password):
+    print("Moderate")
+
+elif len(password) > 10 and any(char.isdigit() for char in password) and any(char.isupper() for char in password):
+    print("Strong")
+
+else:
+    print("Does not meet any strength category")
 
 
 
@@ -52,6 +64,19 @@ else:
 # "Invalid" if it does not start with a capital letter
 # "Invalid" if it does not end with a number
 # "Valid password" otherwise
+password = input("Enter password: ")
+
+if not password[0].isupper():
+    print("Invalid")
+
+elif not password[-1].isdigit():
+    print("Invalid")
+
+else:
+    print("Valid password")
+
+
+
 # 5.
 # Write a program that takes a number and checks:
 
@@ -60,6 +85,19 @@ else:
 # "FizzBuzz" if divisible by both
 # Otherwise print the number
 
+number = int(input("Enter a number: "))
+
+if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+
+elif number % 3 == 0:
+    print("Fizz")
+
+elif number % 5 == 0:
+    print("Buzz")
+
+else:
+    print(number)
 # 6.
 # Create a program that takes a score and prints a grade:
 # A (≥ 80)
@@ -67,18 +105,57 @@ else:
 # C (60–69)
 # D (50–59)
 # F (< 50)
+score = int(input("Enter score: "))
+
+if score >= 80:
+    print("A")
+
+elif 70 <= score <= 79:
+    print("B")
+
+elif 60 <= score <= 69:
+    print("C")
+
+elif 50 <= score <= 59:
+    print("D")
+
+else:
+    print("F")
+
 
 # # 7.
 # # Create a program that takes two numbers and prints:
 # # "Equal" if same
 # # "First is greater"
 # # "Second is greater"
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 == num2:
+    print("Equal")
+
+elif num1 > num2:
+    print("First is greater")
+
+else:
+    print("Second is greater")
 
 # # 8.
 # # Write a program that takes a day number (1–7) and prints:
 # # Weekday (1–5)
 # # Weekend (6–7)
 # # Invalid input otherwise
+
+day = int(input("Enter day number (1-7): "))
+
+if 1 <= day <= 5:
+    print("Weekday")
+
+elif 6 <= day <= 7:
+    print("Weekend")
+
+else:
+    print("Invalid input")
 
 # # 9.
 # # Create a program that takes a temperature and prints:
@@ -87,8 +164,33 @@ else:
 # # "Warm" if 16–30
 # # "Hot" if > 30
 
+temp = int(input("Enter temperature: "))
+
+if temp <= 0:
+    print("Freezing")
+
+elif 1 <= temp <= 15:
+    print("Cold")
+
+elif 16 <= temp <= 30:
+    print("Warm")
+
+else:
+    print("Hot")
+
 # # 10.
 # # Create a program that takes a year and prints:
 # # "Leap year" if divisible by 4
 # # "Century year" if divisible by 100
 # # "Common year" otherwise
+
+year = int(input("Enter a year: "))
+
+if year % 100 == 0:
+    print("Century year")
+
+elif year % 4 == 0:
+    print("Leap year")
+
+else:
+    print("Common year")
